8dbad8bbf02f0e6942e7d4cab33b6414b83cb992
RenatoFrancisco <renatofrancisco.s@gmail.com>
2022-05-29 19:59:46 -0300

    Criando novo projeto

---
+++ 4-ControleFluxo/FrmTelaPrincipal04.Designer.vb
@@ -0,0 +1,68 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmTelaPrincipal04
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.BtnPrincipal = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'BtnPrincipal
+        '
+        Me.BtnPrincipal.Location = New System.Drawing.Point(169, 92)
+        Me.BtnPrincipal.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.BtnPrincipal.Name = "BtnPrincipal"
+        Me.BtnPrincipal.Size = New System.Drawing.Size(179, 95)
+        Me.BtnPrincipal.TabIndex = 0
+        Me.BtnPrincipal.Text = "Clique aqui!"
+        Me.BtnPrincipal.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(21, 31)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(133, 15)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "4-Controle de Fluxo"
+        '
+        'FrmTelaPrincipal04
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(600, 366)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.BtnPrincipal)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Name = "FrmTelaPrincipal04"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Aplicação Alô Mundo"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents BtnPrincipal As Button
+    Friend WithEvents Label1 As Label
+End Class
