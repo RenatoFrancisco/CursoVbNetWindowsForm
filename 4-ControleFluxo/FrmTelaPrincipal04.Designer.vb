6ab753b362f87b860201f4e48e8de864677329d4
RenatoFrancisco <renatofrancisco.s@gmail.com>
2022-05-29 20:28:22 -0300

    Trabalhando com if

--- 4-ControleFluxo/FrmTelaPrincipal04.Designer.vb
+++ 4-ControleFluxo/FrmTelaPrincipal04.Designer.vb
@@ -24,16 +24,21 @@ Partial Class FrmTelaPrincipal04
     Private Sub InitializeComponent()
         Me.BtnPrincipal = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.TxtIdade = New System.Windows.Forms.TextBox()
+        Me.TxtResultado = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TxtPais = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
         'BtnPrincipal
         '
-        Me.BtnPrincipal.Location = New System.Drawing.Point(169, 92)
+        Me.BtnPrincipal.Location = New System.Drawing.Point(24, 96)
         Me.BtnPrincipal.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.BtnPrincipal.Name = "BtnPrincipal"
-        Me.BtnPrincipal.Size = New System.Drawing.Size(179, 95)
+        Me.BtnPrincipal.Size = New System.Drawing.Size(222, 41)
         Me.BtnPrincipal.TabIndex = 0
-        Me.BtnPrincipal.Text = "Clique aqui!"
+        Me.BtnPrincipal.Text = "Checagem da Permissão de Entrada"
         Me.BtnPrincipal.UseVisualStyleBackColor = True
         '
         'Label1
@@ -47,11 +52,59 @@ Partial Class FrmTelaPrincipal04
         Me.Label1.TabIndex = 1
         Me.Label1.Text = "4-Controle de Fluxo"
         '
+        'TxtIdade
+        '
+        Me.TxtIdade.Location = New System.Drawing.Point(24, 61)
+        Me.TxtIdade.Name = "TxtIdade"
+        Me.TxtIdade.Size = New System.Drawing.Size(130, 20)
+        Me.TxtIdade.TabIndex = 2
+        '
+        'TxtResultado
+        '
+        Me.TxtResultado.Location = New System.Drawing.Point(24, 182)
+        Me.TxtResultado.Name = "TxtResultado"
+        Me.TxtResultado.Size = New System.Drawing.Size(564, 20)
+        Me.TxtResultado.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(21, 158)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(72, 15)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Resultado"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(174, 31)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(196, 15)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Está acompanhado dos pais?"
+        '
+        'TxtPais
+        '
+        Me.TxtPais.Location = New System.Drawing.Point(177, 61)
+        Me.TxtPais.Name = "TxtPais"
+        Me.TxtPais.Size = New System.Drawing.Size(69, 20)
+        Me.TxtPais.TabIndex = 2
+        '
         'FrmTelaPrincipal04
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(600, 366)
+        Me.Controls.Add(Me.TxtResultado)
+        Me.Controls.Add(Me.TxtPais)
+        Me.Controls.Add(Me.TxtIdade)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.BtnPrincipal)
         Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
@@ -65,4 +118,9 @@ Partial Class FrmTelaPrincipal04
 
     Friend WithEvents BtnPrincipal As Button
     Friend WithEvents Label1 As Label
+    Friend WithEvents TxtIdade As TextBox
+    Friend WithEvents TxtResultado As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TxtPais As TextBox
 End Class
--- 4-ControleFluxo/FrmTelaPrincipal04.vb
+++ 4-ControleFluxo/FrmTelaPrincipal04.vb
@@ -7,4 +7,29 @@
         ' Add any initialization after the InitializeComponent() call.
 
     End Sub
+
+    Private Sub BtnPrincipal_Click(sender As Object, e As EventArgs) Handles BtnPrincipal.Click
+        TxtResultado.Text = String.Empty
+
+        Dim idade As Integer
+        idade = TxtIdade.Text
+
+        Dim acompanhadoPais As String
+        acompanhadoPais = TxtPais.Text
+
+        If idade >= 18 Then
+            TxtResultado.Text = "A pessoa tem mais de 18 anos. Pode entrar."
+        Else
+            If idade < 16 Then
+                TxtResultado.Text = "A pessoa tem menos de 16 anos e por isso não pode assistir à peça."
+            Else
+                If acompanhadoPais.ToUpper = "S" Then
+                    TxtResultado.Text = "A pessoa tem entre 16 e 18 e está acompanhada dos pais. Pode entrar."
+                Else
+                    TxtResultado.Text = "A pessoa tem entre 16 e 18, mas NÃO está acompanhada dos pais. NÃO pode entrar."
+                End If
+            End If
+        End If
+
+    End Sub
 End Class
